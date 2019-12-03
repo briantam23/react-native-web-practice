95ee6e065d34b28975067a62ce0260208cd8dbdf
briantam23 <briantam23@gmail.com>
2019-12-03 16:16:52 -0500

    Created pokedex hybrid app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Router, Switch, Route } from './routing';
+import Home from './Home';
+import Pokemon from './Pokemon';
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-    </View>
-  );
+class App extends Component {
+  state = {
+    selectedPokemon: null
+  };
+  selectPokemon = selectedPokemon => {
+    this.setState({
+      selectedPokemon
+    });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Home {...props} selectPokemon={this.selectPokemon} />
+              )}
+            />
+            <Route
+              path="/pokemon"
+              render={props => (
+                <Pokemon
+                  {...props}
+                  selectedPokemon={this.state.selectedPokemon}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    padding: 50
   },
 });
+
+
+export default App;
